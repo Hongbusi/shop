e963adf36d7d889049b7fc6baf53b57abdab58a7
Hongbusi <q951686662@qq.com>
2020-03-06 10:53:46 +0800

    修改了Cart.vue

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="cart root">
-        cart
-    </div>
+  <div class="cart root">购物车功能暂未开放</div>
 </template>
+
+<style scoped>
+.cart {
+  margin: 150px auto;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
